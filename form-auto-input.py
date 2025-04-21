a6d07096fe4cd0089f99e3e0dbaeaf1649e890d2
sapporo1101 <71534849+sapporo1101@users.noreply.github.com>
2025-04-21 11:33:12 +0900

    skip listbox and wait for manual input

--- form-auto-input.py
+++ form-auto-input.py
@@ -27,29 +27,38 @@ wait = WebDriverWait(driver, 10)
 try:
     driver.get(url)
 
-
     while True:
         form = wait.until(EC.presence_of_element_located((By.TAG_NAME, "form")))
-        entries = form.find_elements(By.CSS_SELECTOR, "input[class=\"whsOnd zHQkBf\"], div[class=\"Y6Myld\"] > div[role=\"list\"]")
+        entries = form.find_elements(By.CSS_SELECTOR, "input[class=\"whsOnd zHQkBf\"], div[class=\"Y6Myld\"] > div[role=\"list\"], div[role=\"listbox\"]")
         if len(entries) > len(answers):
             print("not enough answers")
             break
+        has_listboxes = False
         for entry in entries:
-            if entry.tag_name == "div":
+            if entry.get_attribute("role") == "listbox":
+                # 選択の場合はスキップ
+                print("選択: ", answers.pop(0))
+                has_listboxes = True
+            elif entry.get_attribute("role") == "list":
                 choices = json.loads(answers.pop(0))
                 checkboxes = entry.find_elements(By.CSS_SELECTOR, "div[role=\"checkbox\"]")
                 for choice in choices:
                     checkboxes[choice-1].click()
-            else:
+            elif entry.tag_name == "input":
                 element = wait.until(EC.element_to_be_clickable(entry))
                 element.send_keys(answers.pop(0))
+            else:
+                print("invalid entry")
+                break
 
         try:
             next = form.find_element(By.CSS_SELECTOR, "div[jsname=\"OCpkoe\"]")
         except:
             print("completed successfully")
             break
-
+        
+        if has_listboxes:
+            input("press enter to continue")
         next.click()
 finally:
     os.kill(driver.service.process.pid, signal.SIGTERM)
